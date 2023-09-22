af6ef0fbb3121ac25b124e2c900e3a338c49c45c
hoki <Hoki010010@gmail.com>
2023-09-22 17:01:14 +0300

    ban/create

--- config.js
+++ config.js
@@ -1,23 +1,81 @@
 const WorkRoles = {
-    Ban: "1149850510338310165",
+    Ban: "1154769569274150955",
 }
 
 const StuffRoles = {
-    Admin: "1153982129357717544",
-    Moderator: "1153982136404148325"
+    Admin: '1154731145552986112',
+    Moderator: '1154731154025488434'
+}
+
+const Utility = {
+    guildName: 'META',
+    guildAvatar: 'https://cdn.discordapp.com/attachments/1129389401630314507/1154763833722802236/metaavatar.png'
 }
 
 const WorkEmbeds = {
+    NotBanYourself: [
+        {
+            "title": "**Вы не можете забанить сами себя!**",
+            "color": 16711680,
+            "footer": {
+                "text": `Сервер: ${Utility.guildName}`,
+                "icon_url": `${Utility.guildAvatar}`
+            }
+        }
+    ],
+    NotBanBot: [
+        {
+            "title": "**Вы не можете забанить бота!**",
+            "color": 16711680,
+            "footer": {
+                "text": `Сервер: ${Utility.guildName}`,
+                "icon_url": `${Utility.guildAvatar}`
+            }
+        }
+    ],
     NotBanRoleHigh: [
         {
             "title": "**Пользователь не был забанен!**",
             "description": "**Причина:** Позиция человека в стаффе выше, чем ваша.",
             "color": 16711680,
             "footer": {
-              "text": "Сервер:"
+                "text": `Сервер: ${Utility.guildName}`,
+                "icon_url": `${Utility.guildAvatar}`
             }
-          }
-    ]
+        }
+    ],
+    NotBanRoleEquals: [
+        {
+            "title": "**Пользователь не был забанен!**",
+            "description": "**Причина:** Позиция человека в стаффе равна вашей.",
+            "color": 16711680,
+            "footer": {
+                "text": `Сервер: ${Utility.guildName}`,
+                "icon_url": `${Utility.guildAvatar}`
+            }
+        }
+    ],
+    BanOk: [
+        {
+            "description": "**Пользователь был забанен**",
+            "color": 7405312,
+            "footer": {
+                "text": `Сервер: ${Utility.guildName}`,
+                "icon_url": `${Utility.guildAvatar}`
+            }
+        }
+    ],
+    BanNo: [
+        {
+            "title": "**Пользователь не был забанен!**",
+            "description": "**Причина:** Пользователь уже в бане.",
+            "color": 16711680,
+            "footer": {
+                "text": `Сервер: ${Utility.guildName}`,
+                "icon_url": `${Utility.guildAvatar}`
+            }
+        }
+    ],
 }
 
 module.exports = { WorkRoles, StuffRoles, WorkEmbeds };
--- src/Commands/Moderation/ban.js
+++ src/Commands/Moderation/ban.js
@@ -23,21 +23,31 @@ module.exports = {
 
     async execute(client, interaction) {
         try {
-            let getUser = interaction.options.get("пользователь");
-            // console.log(interaction.options.get("пользователь").member.roles.highest.idUsers);
-for(let idUsers in StuffRoles) {
-    if (StuffRoles[idUsers] == interaction.options.get("пользователь").member.roles.highest) {
-        interaction.channel.send({ephemeral: true, embeds: WorkEmbeds.NotBanRoleHigh})
-    } else {
-        interaction.reply({
-            content: 'все не работает'
-        })
-    }
-}
+            const getUser = interaction.options.get('пользователь');
 
+            const memberPosition = interaction.member.roles.cache.filter(r => Object.values(StuffRoles).includes(r.id))?.sort((a, b) => b.position - a.position)?.first()?.position || 1;
+            const targetPosition = getUser.member.roles.cache.filter(r => Object.values(StuffRoles).includes(r.id))?.sort((a, b) => b.position - a.position)?.first()?.position || 0;
+
+            if (interaction.user.id === getUser.member.id) {
+                interaction.reply({ embeds: WorkEmbeds.NotBanYourself })
+            } else if (getUser.user.bot === true) {
+                interaction.reply({ embeds: WorkEmbeds.NotBanBot })
+            } else if (memberPosition < targetPosition) {
+                interaction.reply({ embeds: WorkEmbeds.NotBanRoleHigh })
+            } else if (memberPosition == targetPosition) {
+                interaction.reply({ embeds: WorkEmbeds.NotBanRoleEquals })
+            } else if (memberPosition > targetPosition) {
+                const hasRole = (id) => getUser.member.roles.cache.has(id);
+
+                if (hasRole(WorkRoles.Ban)) {
+                    interaction.reply({ embeds: WorkEmbeds.BanNo })
+                } else {
+                    getUser.member.roles.add(WorkRoles.Ban)
+                    interaction.reply({ embeds: WorkEmbeds.BanOk })
+                }
+            }
         } catch (error) {
-            // console.log(`${color.bold.red(`[COMMAND > BAN : ERROR]`)}` + `${error}.`.bgRed);
-            console.log(error);
+            console.log(`${color.bold.red(`[COMMAND > BAN : ERROR]`)}` + `${error}.`.bgRed);
         }
     }
 }
