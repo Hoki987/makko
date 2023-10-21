d4d0bc115cd5e77ff48eb8ba91823858e561b4be
hoki <Hoki010010@gmail.com>
2023-10-22 02:57:10 +0300

    profile/avatar

--- src/Commands/other/profile.js
+++ src/Commands/other/profile.js
@@ -53,15 +53,38 @@ module.exports = {
                     const context = canvas.getContext("2d");
 
                     const background = await Canvas.loadImage(target.user.bannerURL())
+
+                    context.filter = 'blur(50px)';
                     context.drawImage(background, 0, 0, canvas.width, canvas.height)
-                    context.font = '200px Montserrat SemiBold';
+
+
+                    const profile = await Canvas.loadImage((join(__dirname, '..', '..', 'MEDIA', 'profile.png')))
                     const bif = context.getImageData(10, 10, 10, 10).data
-                    const color =  "#" + ((1 << 24) + (bif[0] << 16) + (bif[1] << 8) + bif[2]).toString(16).slice(1);
-                    console.log(color);
+                    const color = "#" + ((1 << 24) + (bif[0] << 16) + (bif[1] << 8) + bif[2]).toString(16).slice(1);
+
+
+
+                    context.filter = 'blur(0px)';
                     context.fillStyle = `${color}`
-                    context.fillText(`${target.user.displayName}`, 305, 945)
+                    context.fillRect(30, 64, 5, 5)
+                    context.drawImage(profile, 0, 0, canvas.width, canvas.height)
+
+                    const profileImages = await Canvas.loadImage((join(__dirname, '..', '..', 'MEDIA', 'icons.png')))
+                    context.filter = 'blur(0px)';
+                    context.drawImage(profileImages, 0, 0, canvas.width, canvas.height)
+
                     const avatar = await Canvas.loadImage(target.displayAvatarURL())
-                    context.drawImage(avatar, 25, 25, 200, 200)
+
+                    context.filter = 'blur(0px)';
+                    context.fillStyle = `${color}`
+                    context.font = '200px Montserrat SemiBold';
+                    context.fillText(`${target.user.displayName}`, 600, 945)
+                    context.beginPath();
+                    context.arc(250, 250, 125, 0, 2 * Math.PI, true);
+                    context.closePath();
+                    context.clip();
+                    context.drawImage(avatar, 75, 75, 300, 300)
+
                     content = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' })
                 }
                 break;
